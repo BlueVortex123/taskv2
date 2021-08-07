373f8c90ee62707eaf314057b5583228ec9916af
BlueVortex123 <duicu.andrei@gmail.com>
2021-08-07 19:58:31 +0300

    add Models and Migrate

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'deadline', 'status'
+    ];
+
+    public function users()
+    {
+        return $this -> belongsTo(User::class, 'user_id','id');
+    }
+}
